0816da0859a74df9777897ecb1cb5ff67fd0c875
milanimcgraw <anjelammcgraw@gmail.com>
2025-07-23 17:22:34 -0500

    updated google cloud footprint/helpery script and notebook

--- Google Cloud Footprint/helper.py
+++ Google Cloud Footprint/helper.py
@@ -1,52 +1,30 @@
 import os
 from dotenv import load_dotenv
-import json
-import base64
-from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
 
-
 def authenticate():
-    return "DLAI_CREDENTIALS", "silicon-comfort-396606"
-    #Load .env
     load_dotenv()
-    
-    #Decode key and store in .JSON
-    SERVICE_ACCOUNT_KEY_STRING_B64 = os.getenv('SERVICE_ACCOUNT_KEY')
-    SERVICE_ACCOUNT_KEY_BYTES_B64 = SERVICE_ACCOUNT_KEY_STRING_B64.encode("ascii")
-    SERVICE_ACCOUNT_KEY_STRING_BYTES = base64.b64decode(SERVICE_ACCOUNT_KEY_BYTES_B64)
-    SERVICE_ACCOUNT_KEY_STRING = SERVICE_ACCOUNT_KEY_STRING_BYTES.decode("ascii")
 
-    SERVICE_ACCOUNT_KEY = json.loads(SERVICE_ACCOUNT_KEY_STRING)
-
-
-    # Create credentials based on key from service account
-    # Make sure your account has the roles listed in the Google Cloud Setup section
-    credentials = Credentials.from_service_account_info(
-        SERVICE_ACCOUNT_KEY,
-        scopes=['https://www.googleapis.com/auth/cloud-platform'])
-
-    if credentials.expired:
-        credentials.refresh(Request())
-    
-    #Set project ID accoridng to environment variable    
-    PROJECT_ID = os.getenv('PROJECT_ID')
-        
-    return credentials, PROJECT_ID
+    # Load service account key from file
+    key_path = "../key/prjct1-466121-beed6eec3f77.json"
+    credentials = Credentials.from_service_account_file(
+        key_path,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"]
+    )
+    project_id = credentials.project_id
+    return credentials, project_id
 
 
-from dotenv import load_dotenv, find_dotenv 
-import os
+# Separate utility to load API key
+from dotenv import load_dotenv, find_dotenv
 
 def load_env():
-    _ = load_dotenv(find_dotenv())
-
+    load_dotenv(find_dotenv())
 
 def load_emaps_api_key(ret_key=True):
     load_env()
     global api_key
     api_key = os.getenv("ELECTRICITY_MAPS_API_KEY")
-    
     if ret_key:
         return api_key
-    return
+
